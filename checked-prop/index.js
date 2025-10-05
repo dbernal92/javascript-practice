a010f65fccf94e023e83c90d5505b0d94b15077e
Darien Bernal <darien.bernal@gmail.com>
2025-10-05 13:54:09 -0400

    Added style; created const using DOM

---
+++ checked-prop/index.js
@@ -0,0 +1,8 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const payResult = document.getElementById("payResult");
+
