4545b7f3ef32559cd0044f96afe8f28e01da3063
Darien Bernal <darien.bernal@gmail.com>
2025-10-05 19:56:41 -0400

    Ternary subscription; if/else payment

--- checked-prop/index.js
+++ checked-prop/index.js
@@ -6,3 +6,23 @@ const mySubmit = document.getElementById("mySubmit");
 const subResult = document.getElementById("subResult");
 const payResult = document.getElementById("payResult");
 
+mySubmit.onclick = function() {
+    // if (myCheckBox.checked) {
+    //     subResult.textContent = "You are subscribed!"
+    // } else {
+    //     subResult.textContent = "You are not subscribed!"
+    // }
+    subResult.textContent = myCheckBox.checked
+        ? "You're subcribed!"
+        : "You're not subscribed!";
+
+    if (visaBtn.checked) {
+        payResult.textContent = "You're paying with Visa.";
+    } else if (masterCardBtn.checked) {
+        payResult.textContent = "You're paying with MasterCard.";
+    } else if (payPalBtn.checked) {
+        payResult.textContent = "You're paying with PayPal.";
+    } else {
+        payResult.textContent = "Please select a payment method.";
+    }
+};
